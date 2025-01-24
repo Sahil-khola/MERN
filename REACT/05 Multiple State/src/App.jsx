35921c80817c55b069029980ffa8ea2fc0a31c78
Sahil-khola <blackiayaduvanshia@gmail.com>
2025-01-24 11:22:13 +0530

    MERN

--- REACT/05 Multiple State/src/App.jsx
+++ REACT/05 Multiple State/src/App.jsx
@@ -2,34 +2,45 @@ import { useState } from "react"
 
 
 function App() {
-  const [name,setName]=useState("")
-  const [pass,setPass]=useState("")
-  const [email,setEmail]=useState("")
-  function handelName(event) {
-    setName(event.target.value)
-  }
-  function handelPass(event) {
-    setPass(event.target.value)
-  }
-  function handelEmail(event) {
-    setEmail(event.target.value)
+  // const [name,setName]=useState("")
+  // const [pass,setPass]=useState("")
+  // const [email,setEmail]=useState("")
+  // function handelName(event) {
+  //   setName(event.target.value)
+  // }
+  // function handelPass(event) {
+  //   setPass(event.target.value)
+  // }
+  // function handelEmail(event) {
+  //   setEmail(event.target.value)
+  // }
+
+  const [form,setForm]=useState({
+    name:'',
+    email:'',
+    pass:''
+  })
+
+  function handelInput(e) {
+        const {name,value}=e.target;
+   setForm({...form,[name]:value,})
   }
   return (
     <>
      <form>
       <label htmlFor="Name">Name</label>
-      <input type="text" value={name} onChange={handelName}  id="Name" />
+      <input type="text" value={form.name} onChange={handelInput} name="name"  id="Name" />
       <br /><br />
       <label htmlFor="Email">Email</label>
-      <input type="Email" value={email} onChange={handelEmail} id="Email"/>
+      <input type="Email" value={form.email} onChange={handelInput} name="email" id="Email"/>
       <br /><br />
       <label htmlFor="Password">Password</label>
-      <input type="password" value={pass} onChange={handelPass} id="Password" />
-      <button onClick={()=>{setEmail([]),setName([]),setPass([])}}>Clear</button>
+      <input type="password" value={form.pass} onChange={handelInput} name="pass" id="Password" />
+      {/* <button onClick={()=>{setEmail([]),setName([]),setPass([])}}>Clear</button> */}
       <pre>
-        Name :{name} <br />
-        Email :{email} <br />
-        Password :{pass}
+        Name :{form.name} <br />
+        Email :{form.email} <br />
+        Password :{form.pass}
       </pre>
      </form>
     </>
