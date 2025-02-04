afd5f44bf42139719fdb474764124255cf40df60
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-02-04 21:11:45 +0530

    MERN

--- REACT/16 useTransition/src/App.jsx
+++ REACT/16 useTransition/src/App.jsx
@@ -1,9 +1,16 @@
+import { useTransition } from "react"
 
 function App() {
-
+ 
+  const [pending ,startTransistion] = useTransition();
+   function handelbutton() {
+    startTransistion(async()=>{
+      await new Promise (res =>setTimeout(res,2000))
+    })
+  }
   return (
     <>
-  
+   <button disabled={pending} onClick={handelbutton}>Click me</button>
     </>
   )
 }
